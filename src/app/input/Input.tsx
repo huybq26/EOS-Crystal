36af0150a9c99b9d11b8a59298e2b0403611bbbe
huybq26 <81632425+huybq26@users.noreply.github.com>
2021-05-25 03:23:08 +0800

    Minor changes in type traverse autofill

--- src/app/input/Input.tsx
+++ src/app/input/Input.tsx
@@ -1,111 +1,225 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import {
   Button,
-  Paper,
-  Step,
-  StepLabel,
-  Stepper,
   Typography,
+  Grid,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
 } from '@material-ui/core';
-import { InputStyles } from './Input.styles';
-import UploadFile from './UploadFile';
-import ConfirmData from './ConfirmData';
-import { excelToJson } from '../../utils/ExcelToJson';
-import _ from 'lodash';
+import { makeStyles } from '@material-ui/core/styles';
 
-const steps = ['Upload File', 'Confirm Data'];
+const crystalNameInput = 'crystalName';
+const mineralNameInput = 'mineralName';
+const volcanoNameInput = 'volcanoName';
+const eruptionYearInput = 'eruptionYear';
+const referenceInput = 'referenceInput';
+const typeOfCrystalInput = false; // to be modified
 
-export default function Input() {
-  const classes = InputStyles();
+const authorInput = 'authorInput';
+const titleInput = 'titleInput';
+const yearInput = '2000';
+const doiInput = 'doiInput';
 
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [excelFile, setExcelFile] = React.useState<File | null>(null);
-  const [json, setJsonFile] = React.useState({});
-  const [submitted, setSubmitted] = React.useState(false);
+const useStyles = makeStyles((theme) => ({
+  crystalType: {
+    marginTop: theme.spacing(5),
+  },
+  grid2: {
+    // marginLeft: theme.spacing(1)
+  },
+  gridBtn: {
+    marginTop: theme.spacing(3),
+  },
+  chooseFiles: {},
+  submitBtn: {
+    // marginLeft: theme.spacing(30)
+  },
+}));
 
-  const onExcelFileChange = (newFile: File | null): void => {
-    setExcelFile(newFile);
-    if (newFile) {
-      excelToJson(newFile).then((data) => {
-        setJsonFile(data);
-      });
+type ConfirmDataProps = {
+  mineral: string;
+  crystalName: string;
+  volcanoName: string;
+  eruptionYear: string;
+  reference: string;
+  crystalType: string;
+};
+
+export default function ConfirmData(props: ConfirmDataProps) {
+  const {
+    mineral,
+    crystalName,
+    volcanoName,
+    eruptionYear,
+    reference,
+    crystalType,
+  } = props;
+  //alert(props.mineral);
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: crystalType == 'Rim-to-Rim ' ? true : false,
+    checkedB: false,
+    checkedC: false,
+  });
+  const [crystalNameInput, setName] = React.useState('');
+  const [mineralNameInput, setMineralNameInput] = React.useState('');
+  const [volcanoNameInput, setVolcanoNameInput] = React.useState('');
+  const [eruptionYearInput, setEruptionYearInput] = React.useState('');
+  const [referenceInput, setReferenceInput] = React.useState('');
+  const [typeofCrystal, setType] = React.useState('');
+  const [author, setAuthor] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [year, setYear] = React.useState('');
+  const [doi, setDOI] = React.useState('');
+  const formRef = React.useRef(null);
+
+  const handleChange = (event: any) => {
+    let name = event.target.name;
+    if (name === crystalNameInput) {
+      setName(event.target.value);
+    }
+    //else if (name === typeOfCrystalInput) {
+    //     setType(event.target.value);
+    //}
+    else if (name === authorInput) {
+      setAuthor(event.target.value);
+    } else if (name === titleInput) {
+      setTitle(event.target.value);
+    } else if (name === yearInput) {
+      setYear(event.target.value);
+    } else if (name === doiInput) {
+      setDOI(event.target.value);
     }
   };
 
-  const handleNext = (): void => {
-    setActiveStep(activeStep + 1);
-    setSubmitted(true);
+  const handleCheckBoxChange = (event: any) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
   };
-
-  const handleBack = (): void => {
-    setActiveStep(activeStep - 1);
-    setSubmitted(false);
-  };
-
-  const disableNext = (): boolean => {
-    if (activeStep == 0 && excelFile == null) {
-      return true;
-    }
-
-    return false;
-  };
-
-  let content = null;
-
-  if (submitted) {
-    let jsonFile = JSON.parse(JSON.stringify(json));
-    content = (
-      <ConfirmData
-        mineral={jsonFile['mineral']}
-        crystalName={jsonFile['crystal name']}
-        volcanoName={jsonFile.volcano}
-        eruptionYear={jsonFile.eruption}
-        reference={jsonFile.reference}
-      />
-    );
-  }
 
   return (
-    <Paper className={classes.paper}>
-      <Typography component='h1' variant='h5' align='center'>
-        New Crystal Data
-      </Typography>
+    <div>
+      <Grid container spacing={1}>
+        <Grid item sm>
+          <FormGroup /* className= {classes.formLabel}*/>
+            <TextField
+              onChange={handleChange}
+              name={crystalNameInput}
+              margin='normal'
+              id='crystal-name'
+              //label='Crystal name'
+              helperText='Crystal name'
+              placeholder={props.crystalName}
+            ></TextField>
+            <TextField
+              onChange={handleChange}
+              name={mineralNameInput}
+              margin='normal'
+              id='crystal-name'
+              //   label='Mineral name'
+              helperText='Mineral name'
+              placeholder={props.mineral}
+            ></TextField>
+            <TextField
+              onChange={handleChange}
+              name={volcanoNameInput}
+              margin='normal'
+              id='crystal-name'
+              //label='Volcano name'
+              helperText='Volcano name'
+              placeholder={props.volcanoName}
+            ></TextField>
 
-      <Stepper activeStep={activeStep} className={classes.stepper}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
+            <Typography className={classes.crystalType}>
+              Type traverse
+            </Typography>
 
-      <React.Fragment>
-        <div style={{ position: 'relative' }}>
-          <UploadFile
-            step={0}
-            activeStep={activeStep}
-            excelFile={excelFile}
-            excelFileChange={onExcelFileChange}
-          />
-        </div>
-        {content}
-        <div className={classes.buttons}>
-          {activeStep !== 0 && (
-            <Button onClick={handleBack} className={classes.button}>
-              Back
-            </Button>
-          )}
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={handleNext}
-            className={classes.button}
-            disabled={disableNext()}
-          >
-            {activeStep === steps.length - 1 ? 'Save' : 'Next'}
-          </Button>
-        </div>
-      </React.Fragment>
-    </Paper>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.checkedA}
+                  onChange={handleCheckBoxChange}
+                  name='checkedA'
+                  color='primary'
+                />
+              }
+              label='R-to-R'
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.checkedB}
+                  onChange={handleCheckBoxChange}
+                  name='checkedB'
+                  color='primary'
+                />
+              }
+              label='R-to-C'
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.checkedC}
+                  onChange={handleCheckBoxChange}
+                  name='checkedC'
+                  color='primary'
+                />
+              }
+              label='C-to-C'
+            />
+          </FormGroup>
+        </Grid>
+
+        <Grid item sm className={classes.grid2}>
+          <FormGroup>
+            <TextField
+              // onChange={handleChange}
+              // name={eruptionYearInput}
+              margin='normal'
+              id='crystal-name'
+              //label={props.eruptionYear}
+              helperText='Eruption year'
+              placeholder={props.eruptionYear}
+            ></TextField>
+            <TextField
+              onChange={handleChange}
+              name={referenceInput}
+              fullWidth
+              margin='normal'
+              id='author-name'
+              //label='Reference'
+              helperText='Reference'
+              placeholder={props.reference}
+            ></TextField>
+
+            <TextField
+              onChange={handleChange}
+              name={titleInput}
+              fullWidth
+              margin='normal'
+              id='crystal-name'
+              label='Research Title'
+            ></TextField>
+            <TextField
+              onChange={handleChange}
+              name={yearInput}
+              fullWidth
+              margin='normal'
+              id='year-publish'
+              label='Published year'
+            ></TextField>
+            <TextField
+              onChange={handleChange}
+              name={doiInput}
+              fullWidth
+              margin='normal'
+              id='doi'
+              label='Digital Object Identifier'
+            ></TextField>
+          </FormGroup>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
